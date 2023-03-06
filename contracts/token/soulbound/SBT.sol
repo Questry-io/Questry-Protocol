147a9e83fcd4d6e5ef29382e3a9394169493da5f
motxx <motohiko.ave@gmail.com>
2023-03-06 12:36:17 +0900

    fix missing variable replacement

--- contracts/token/soulbound/SBT.sol
+++ contracts/token/soulbound/SBT.sol
@@ -56,7 +56,7 @@ contract SBT is
     function tokenURI(uint256 tokenId) public view virtual override(ERC721) returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
         string memory baseURI = _baseURI();
-        return bytes(baseURI).length > 0 ? string(abi.encodePacked(_baseURI(), tokenId.toString(), ".json")) : _DefaultURI;
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json")) : _DefaultURI;
     }
 
     function _baseURI() internal view virtual override(ERC721) returns (string memory) {
