e34e65ecd93c3f50a4aacee37dd45ba6bccf2258
somasekimoto <0421.soma@gmail.com>
2023-04-03 15:55:19 +0900

    remove: MinimalForwarder & create: custom Forwarder functions

--- contracts/token/forwarder/QuestryForwarder.sol
+++ contracts/token/forwarder/QuestryForwarder.sol
@@ -5,16 +5,31 @@ import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/metatx/MinimalForwarderUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
 
 contract QuestryForwarder is
   Initializable,
   PausableUpgradeable,
   AccessControlUpgradeable,
   UUPSUpgradeable,
-  MinimalForwarderUpgradeable
+  EIP712Upgradeable
 {
+  using ECDSAUpgradeable for bytes32;
+  bytes32 private constant _TYPEHASH =
+    keccak256(
+      "ForwardRequest(address from,address to,uint256 value,uint256 gas,uint256 nonce,bytes data)"
+    );
   bytes32 public constant EXECUTOR_ROLE = keccak256("EXECUTOR_ROLE");
+
+  struct ForwardRequest {
+    address from;
+    address to;
+    uint256 value;
+    uint256 gas;
+    uint256 nonce;
+    bytes data;
+  }
 
   mapping(address => uint256) private _nonces;
 
@@ -22,15 +37,13 @@ contract QuestryForwarder is
   event Withdraw(address indexed sender, uint256 value);
 
   /// @custom:oz-upgrades-unsafe-allow constructor
-  constructor() {
-    // _disableInitializers();
-  }
+  constructor() {}
 
   function initialize(address _admin, address _executor) public initializer {
     __Pausable_init();
     __AccessControl_init();
     __UUPSUpgradeable_init();
-    __MinimalForwarder_init();
+    __EIP712_init("QuestryForwarder", "0.0.1");
 
     _grantRole(DEFAULT_ADMIN_ROLE, _admin);
     _grantRole(EXECUTOR_ROLE, _executor);
@@ -60,16 +73,62 @@ contract QuestryForwarder is
     emit Withdraw(_executor, _amount);
   }
 
-  function customExecute(ForwardRequest calldata req, bytes calldata signature)
+  function getNonce(address from) public view returns (uint256) {
+    return _nonces[from];
+  }
+
+  function verify(ForwardRequest calldata req, bytes calldata signature)
+    public
+    view
+    returns (bool)
+  {
+    address signer = _hashTypedDataV4(
+      keccak256(
+        abi.encode(
+          _TYPEHASH,
+          req.from,
+          req.to,
+          req.value,
+          req.gas,
+          req.nonce,
+          keccak256(req.data)
+        )
+      )
+    ).recover(signature);
+    return _nonces[req.from] == req.nonce && signer == req.from;
+  }
+
+  function execute(ForwardRequest calldata req, bytes calldata signature)
     public
     payable
     onlyRole(EXECUTOR_ROLE)
     whenNotPaused
     returns (bool, bytes memory)
   {
+    require(
+      verify(req, signature),
+      "MinimalForwarder: signature does not match request"
+    );
     uint256 startGas = gasleft();
+    _nonces[req.from] = req.nonce + 1;
 
-    (bool success, bytes memory returndata) = execute(req, signature);
+    (bool success, bytes memory returndata) = req.to.call{
+      gas: req.gas,
+      value: req.value
+    }(abi.encodePacked(req.data, req.from));
+
+    // Validate that the relayer has sent enough gas for the call.
+    // See https://ronan.eth.limo/blog/ethereum-gas-dangers/
+    if (gasleft() <= req.gas / 63) {
+      // We explicitly trigger invalid opcode to consume all gas and bubble-up the effects, since
+      // neither revert or assert consume all gas since Solidity 0.8.0
+      // https://docs.soliditylang.org/en/v0.8.0/control-structures.html#panic-via-assert-and-error-via-require
+      /// @solidity memory-safe-assembly
+      assembly {
+        invalid()
+      }
+    }
+
     if (!success) {
       revert("QuestryForwarder: execute reverted");
     }
