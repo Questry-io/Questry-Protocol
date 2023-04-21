d823bcb3fea765ab20916002138c70828db00a53
motxx <motohiko.ave@gmail.com>
2023-04-21 16:25:39 +0900

    style: apply prettier to SBT files

--- contracts/Factory/SBTFactory.sol
+++ contracts/Factory/SBTFactory.sol
@@ -1,55 +1,79 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import { SBT, AccessControl } from "../token/soulbound/SBT.sol";
+import {SBT, AccessControl} from "../token/soulbound/SBT.sol";
 
 contract SBTFactory is AccessControl {
-    event SBTCreated(address contractAddress, string name, string symbol, address indexed pjmanager, address indexed admin);
-    
-    bytes32 public constant SET_FORWARDER_ROLE = keccak256("SET_FORWARDER_ROLE");
+  event SBTCreated(
+    address contractAddress,
+    string name,
+    string symbol,
+    address indexed pjmanager,
+    address indexed admin
+  );
 
-    constructor(
-        address admin
-    ) {
-        _setupRole(DEFAULT_ADMIN_ROLE, admin);
-        _setupRole(SET_FORWARDER_ROLE, admin);
-    }
-    // Mapping from name and symbol to basic ERC721 address
-    mapping(string => mapping(string => address)) public getSBTaddress;
+  bytes32 public constant SET_FORWARDER_ROLE = keccak256("SET_FORWARDER_ROLE");
 
-    address private _TrustedForwarder;
+  constructor(address admin) {
+    _setupRole(DEFAULT_ADMIN_ROLE, admin);
+    _setupRole(SET_FORWARDER_ROLE, admin);
+  }
 
-    function createSBT(
-        string calldata _name, 
-        string calldata _symbol,
-        string memory _baseTokenURI,
-        address _pjmanager,
-        address _admin
-    ) external returns (address sbt) {
-        require(getSBTaddress[_name][_symbol] == address(0), "SBTFactory: must use another name and symbol");
+  // Mapping from name and symbol to basic ERC721 address
+  mapping(string => mapping(string => address)) public getSBTaddress;
 
-        bytes32 _salt = keccak256(abi.encodePacked(_name, _symbol));
-        sbt = address(new SBT{salt: _salt}(_name, _symbol, _baseTokenURI, _pjmanager, _admin, _TrustedForwarder));
+  address private _TrustedForwarder;
 
-        getSBTaddress[_name][_symbol] = sbt;
-        emit SBTCreated(sbt, _name, _symbol, _pjmanager, _admin);
-    }
-    //Same _name & _symbol let be override
-    function getContractAddress(string calldata _name,string calldata _symbol) public view returns (address) {
-        return getSBTaddress[_name][_symbol];
-    }
+  function createSBT(
+    string calldata _name,
+    string calldata _symbol,
+    string memory _baseTokenURI,
+    address _pjmanager,
+    address _admin
+  ) external returns (address sbt) {
+    require(
+      getSBTaddress[_name][_symbol] == address(0),
+      "SBTFactory: must use another name and symbol"
+    );
 
-    function setChildTrustedforwarder(address ForwarderAddress) public {
-        require(hasRole(SET_FORWARDER_ROLE, _msgSender()), "SBTFactory: must have SET_FORWARDER_ROLE");
-        _setChildTrusedForwarder(ForwarderAddress);
-    }
+    bytes32 _salt = keccak256(abi.encodePacked(_name, _symbol));
+    sbt = address(
+      new SBT{salt: _salt}(
+        _name,
+        _symbol,
+        _baseTokenURI,
+        _pjmanager,
+        _admin,
+        _TrustedForwarder
+      )
+    );
 
-    function _setChildTrusedForwarder(address ForwarderAddress) internal {
-        _TrustedForwarder = ForwarderAddress;
-    }
+    getSBTaddress[_name][_symbol] = sbt;
+    emit SBTCreated(sbt, _name, _symbol, _pjmanager, _admin);
+  }
 
-    function getChildTrustedforwarder() public view returns (address) {
-        return _TrustedForwarder;
-    }
+  //Same _name & _symbol let be override
+  function getContractAddress(string calldata _name, string calldata _symbol)
+    public
+    view
+    returns (address)
+  {
+    return getSBTaddress[_name][_symbol];
+  }
 
+  function setChildTrustedforwarder(address ForwarderAddress) public {
+    require(
+      hasRole(SET_FORWARDER_ROLE, _msgSender()),
+      "SBTFactory: must have SET_FORWARDER_ROLE"
+    );
+    _setChildTrusedForwarder(ForwarderAddress);
+  }
+
+  function _setChildTrusedForwarder(address ForwarderAddress) internal {
+    _TrustedForwarder = ForwarderAddress;
+  }
+
+  function getChildTrustedforwarder() public view returns (address) {
+    return _TrustedForwarder;
+  }
 }
