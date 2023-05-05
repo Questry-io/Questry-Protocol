33e486e6075debc76a70bdda78fd0a5d7c348511
motxx <motohiko.ave@gmail.com>
2023-05-05 11:02:31 +0900

    KPV0-96 feat: support meta-tx for PJManagerFactory

--- contracts/factory/PJManagerFactory.sol
+++ contracts/factory/PJManagerFactory.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import {Context, ERC2771Context} from "@openzeppelin/contracts/metatx/ERC2771Context.sol";
 import {IPJManagerFactory} from "../interface/factory/IPJManagerFactory.sol";
 import {IPJManager} from "../interface/pjmanager/IPJManager.sol";
 import {LibPJManager} from "../library/LibPJManager.sol";
@@ -8,14 +9,16 @@ import {PJManager} from "../pjmanager/PJManager.sol";
 import {QuestryPlatform} from "../platform/QuestryPlatform.sol";
 import {SBT, AccessControl} from "../token/soulbound/SBT.sol";
 
-contract PJManagerFactory is IPJManagerFactory, AccessControl {
+contract PJManagerFactory is IPJManagerFactory, AccessControl, ERC2771Context {
   event PJManagerCreated(address indexed businessOwner, address pjManager);
 
   QuestryPlatform public immutable questryPlatform;
   mapping(address => IPJManager[]) public pjManagersByAdmin;
   mapping(IPJManager => address) public adminByPJManager;
 
-  constructor(QuestryPlatform _questryPlatform) {
+  constructor(QuestryPlatform _questryPlatform, address _trustedForwarder)
+    ERC2771Context(_trustedForwarder)
+  {
     questryPlatform = _questryPlatform;
   }
 
@@ -55,5 +58,29 @@ contract PJManagerFactory is IPJManagerFactory, AccessControl {
     returns (IPJManager[] memory)
   {
     return pjManagersByAdmin[businessOwner];
+  }
+
+  /**
+   * @dev See {ERC2771Context-_msgSender}
+   */
+  function _msgSender()
+    internal
+    view
+    override(Context, ERC2771Context)
+    returns (address sender)
+  {
+    sender = ERC2771Context._msgSender();
+  }
+
+  /**
+   * @dev See {ERC2771Context-_msgData}
+   */
+  function _msgData()
+    internal
+    view
+    override(Context, ERC2771Context)
+    returns (bytes calldata)
+  {
+    return ERC2771Context._msgData();
   }
 }
--- test/factory/PJManagerFactory.test.ts
+++ test/factory/PJManagerFactory.test.ts
@@ -1,43 +1,181 @@
 /* eslint-disable node/no-missing-import */
-import { ethers } from "hardhat";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { PJManagerFactory } from "../../typechain";
+import { ethers, upgrades } from "hardhat";
 import { expect } from "chai";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { getMetaTx, getMetaTxAndSignForGas } from "../utils";
+import { PJManagerFactory, QuestryForwarder } from "../../typechain";
 
 describe("PJManagerFactory", function () {
-  let deployer: SignerWithAddress;
-  let superAdmin: SignerWithAddress;
+  let admin: SignerWithAddress;
+  let issuer: SignerWithAddress;
+  let executor: SignerWithAddress;
   let pjManagerAdmin: SignerWithAddress;
-  let cPJManagerFactory: PJManagerFactory;
-
-  const dummyContract = "0x00E9C198af8F6a8692d83d1702e691A03F2cdc63";
+  let contract: PJManagerFactory;
+  let forwarderContract: QuestryForwarder;
 
   beforeEach(async function () {
-    [deployer, superAdmin, pjManagerAdmin] = await ethers.getSigners();
+    [admin, issuer, executor, pjManagerAdmin] = await ethers.getSigners();
 
-    const cfPJManagerFactory = await ethers.getContractFactory(
+    const QuestryForwarder = await ethers.getContractFactory(
+      "QuestryForwarder"
+    );
+    forwarderContract = (await upgrades.deployProxy(
+      QuestryForwarder,
+      [admin.address, executor.address],
+      { initializer: "initialize" }
+    )) as QuestryForwarder;
+    await forwarderContract.deployed();
+
+    const PJManagerFactory = await ethers.getContractFactory(
       "PJManagerFactory"
     );
-    cPJManagerFactory = await cfPJManagerFactory.deploy(dummyContract);
-    await cPJManagerFactory.deployed();
+    contract = await PJManagerFactory.deploy(
+      ethers.constants.AddressZero,
+      forwarderContract.address
+    );
+    await contract.deployed();
+
+    await admin.sendTransaction({
+      to: forwarderContract.address,
+      value: ethers.utils.parseEther("1.0"),
+    });
+
+    this.name = "QuestryForwarder";
+    this.chainId = (await ethers.provider.getNetwork()).chainId;
+    this.value = "0";
+    this.gas = (await ethers.provider.getBlock("latest")).gasLimit.toString();
   });
 
   describe("createPJManager", function () {
     it("[S] should createPJManager", async function () {
-      const tx = await cPJManagerFactory
+      const tx = await contract
         .connect(pjManagerAdmin)
         .createPJManager(10000, []);
-      const pjManagers = await cPJManagerFactory.getPJManagers(
-        pjManagerAdmin.address
-      );
+      const pjManagers = await contract.getPJManagers(pjManagerAdmin.address);
       expect(pjManagers.length).to.equal(1);
       expect(pjManagers[0]).not.equal(ethers.constants.AddressZero);
-      expect(await cPJManagerFactory.getPJManagerAdmin(pjManagers[0])).to.equal(
+      expect(await contract.getPJManagerAdmin(pjManagers[0])).to.equal(
         pjManagerAdmin.address
       );
       expect(tx)
-        .to.emit(cPJManagerFactory, "PJManagerCreated")
+        .to.emit(contract, "PJManagerCreated")
         .withArgs(pjManagerAdmin.address, pjManagers[0]);
+    });
+  });
+
+  describe("metatx", function () {
+    describe("execute", function () {
+      it("[S] should process the meta-transaction correctly", async function () {
+        // Prepare meta-transaction
+        const data = contract.interface.encodeFunctionData("createPJManager", [
+          10000,
+          [],
+        ]);
+        const from = issuer.address;
+        const nonce: string = (
+          await forwarderContract.getNonce(from)
+        ).toString();
+        const to = contract.address;
+        // get proper gas to execute meta tx
+        const { metaTxForGas, signForGas } = await getMetaTxAndSignForGas(
+          this.name,
+          this.chainId,
+          forwarderContract.address,
+          from,
+          to,
+          nonce,
+          data,
+          this.value,
+          this.gas
+        );
+        const estimatedGas: string = (
+          await forwarderContract
+            .connect(executor)
+            .estimateGas.execute(metaTxForGas.message, signForGas)
+        ).toString();
+        // create typedData for sign meta tx
+        const metaTx = await getMetaTx(
+          this.name,
+          this.chainId,
+          forwarderContract.address,
+          from,
+          to,
+          nonce,
+          data,
+          this.value,
+          estimatedGas
+        );
+        // sign meta tx
+        const signature = await ethers.provider.send("eth_signTypedData_v4", [
+          from,
+          JSON.stringify(metaTx),
+        ]);
+        expect(
+          await forwarderContract.verify(metaTx.message, signature)
+        ).to.equal(true);
+
+        // Relay meta-transaction
+        await forwarderContract
+          .connect(executor)
+          .execute(metaTx.message, signature);
+        // Check if the meta-transaction was processed successfully
+        expect(await forwarderContract.getNonce(from)).to.equal(nonce + 1);
+      });
+
+      it("[R] should revert when the signature is invalid", async function () {
+        const data = contract.interface.encodeFunctionData("createPJManager", [
+          10000,
+          [],
+        ]);
+        const nonce = (
+          await forwarderContract.getNonce(issuer.address)
+        ).toString();
+        const from = issuer.address;
+        const to = contract.address;
+        // get proper gas to execute meta tx
+        const { metaTxForGas, signForGas } = await getMetaTxAndSignForGas(
+          this.name,
+          this.chainId,
+          forwarderContract.address,
+          from,
+          to,
+          nonce,
+          data,
+          this.value,
+          this.gas
+        );
+        const estimatedGas: string = (
+          await forwarderContract
+            .connect(executor)
+            .estimateGas.execute(metaTxForGas.message, signForGas)
+        ).toString();
+        // create typedData for sign meta tx
+        const metaTx = await getMetaTx(
+          this.name,
+          this.chainId,
+          forwarderContract.address,
+          from,
+          to,
+          nonce,
+          data,
+          this.value,
+          estimatedGas
+        );
+        const signature = await ethers.provider.send("eth_signTypedData_v4", [
+          metaTx.message.from,
+          JSON.stringify(metaTx),
+        ]);
+
+        // Sign meta-transaction with incorrect nonce
+        // Relay meta-transaction
+        await expect(
+          forwarderContract
+            .connect(executor)
+            .execute({ ...metaTx.message, nonce: 10 }, signature)
+        ).to.be.revertedWith(
+          "QuestryForwarder: signature does not match request"
+        );
+      });
     });
   });
 });
