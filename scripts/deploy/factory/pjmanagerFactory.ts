ee05c5f8814d28bd92b6966831a5d3cb84bc37c4
somasekimoto <0421.soma@gmail.com>
2023-05-09 18:54:39 +0900

    remove comment out

--- scripts/deploy/factory/pjmanagerFactory.ts
+++ scripts/deploy/factory/pjmanagerFactory.ts
@@ -6,11 +6,6 @@
 import * as hre from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
   await hre.run("compile");
   const questryPlatformAddress = "";
   if (questryPlatformAddress == "") {
